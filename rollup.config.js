dbad01289947e8be6d9c716f7ec1bdc3447f003a
MalwareX95 <graphandfun@hotmail.com>
2020-04-09 19:12:50 +0200

    x

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
-import { createDefaultConfig } from '@open-wc/building-rollup/modern-and-legacy-config';
+import { createDefaultConfig } from '@open-wc/building-rollup';
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import { createCompatibilityConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './index.html', extensions: ['.js', '.mjs', '.ts']});
+export default createDefaultConfig({ 
+    input: './index.html', 
+    extensions: ['.js', '.mjs', '.ts']
+});
