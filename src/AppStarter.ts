cb904ffd4277baa9256089a1835b0a7da739b3f6
MalwareX95 <graphandfun@hotmail.com>
2020-04-09 18:30:54 +0200

    x

--- src/AppStarter.ts
+++ src/AppStarter.ts
@@ -1,81 +1,82 @@
 import { LitElement, html, css, customElement } from 'lit-element';
 import { openWcLogo } from './open-wc-logo.js';
 
-@customElement('app-starter')
-export class AppStarter extends LitElement {
+console.log('1233')
 
-  static get properties() {
-    return {
-      title: { type: String },
-      page: { type: String },
-    };
-  }
+// export class AppStarter extends LitElement {
 
-  static get styles() {
-    return css`
-      :host {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
-      }
+//   static get properties() {
+//     return {
+//       title: { type: String },
+//       page: { type: String },
+//     };
+//   }
 
-      main {
-        flex-grow: 1;
-      }
+//   static get styles() {
+//     return css`
+//       :host {
+//         min-height: 100vh;
+//         display: flex;
+//         flex-direction: column;
+//         align-items: center;
+//         justify-content: flex-start;
+//         font-size: calc(10px + 2vmin);
+//         color: #1a2b42;
+//         max-width: 960px;
+//         margin: 0 auto;
+//         text-align: center;
+//       }
 
-      .logo > svg {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
+//       main {
+//         flex-grow: 1;
+//       }
 
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
+//       .logo > svg {
+//         margin-top: 36px;
+//         animation: app-logo-spin infinite 20s linear;
+//       }
 
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
+//       @keyframes app-logo-spin {
+//         from {
+//           transform: rotate(0deg);
+//         }
+//         to {
+//           transform: rotate(360deg);
+//         }
+//       }
 
-      .app-footer a {
-        margin-left: 5px;
-      }
-    `;
-  }
+//       .app-footer {
+//         font-size: calc(12px + 0.5vmin);
+//         align-items: center;
+//       }
 
-  render() {
-    return html`
-      <main>
-        <div class="logo">${openWcLogo}</div>
-        <h1>My</h1>
+//       .app-footer a {
+//         margin-left: 5px;
+//       }
+//     `;
+//   }
 
-        <p>Edit <code>src/MyApp.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/developing/#code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
+//   render() {
+//     return html`
+//       <main>
+//         <div class="logo">${openWcLogo}</div>
+//         <h1>My</h1>
 
-      <p class="app-footer">
-        🚽 Made with love by
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/open-wc">open-wc</a>.
-      </p>
-    `;
-  }
-}
+//         <p>Edit <code>src/MyApp.js</code> and save to reload.</p>
+//         <a
+//           class="app-link"
+//           href="https://open-wc.org/developing/#code-examples"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Code examples
+//         </a>
+//       </main>
+
+//       <p class="app-footer">
+//         🚽 Made with love by
+//         <a target="_blank" rel="noopener noreferrer" href="https://github.com/open-wc">open-wc</a>.
+//       </p>
+//     `;
+//   }
+// }
